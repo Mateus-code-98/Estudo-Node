b5b181bb13480167e96de89c5a636fc527ddf34a
Mateus-code-98 <teu-9@hotmail.com>
2021-06-01 21:57:06 -0300

    Adição de tratativas de erros na verificação do token JWT informado pelo usuário

--- base_api/src/middlewares/userAuthenticated.js
+++ base_api/src/middlewares/userAuthenticated.js
@@ -1,21 +1,26 @@
 const { verify } = require('jsonwebtoken')
+const AppError = require('../errors/AppError')
 
 const UserAuthenticated = (req,res,next) => {
     const authHeader = req.headers.authorization
 
     // Verifica se o token JWT foi enviado no cabeçalho da requisição
-    if(!authHeader) return res.json({message:"JWT token is missing!"})
+    if(!authHeader) throw new AppError('JWT token is missing','400')
 
     // Retira o 'Bearer' que vem antes do token JWT no atributo 'authorization' do cabeçalho da requisição
     const token = authHeader.split(' ')[1]
 
     //Verifica se o token JWT é válido
-    const decoded = verify(token,process.env.JWT_SECRET)
+    try{
+        const decoded = verify(token,process.env.JWT_SECRET)
+        // Adiciona à requisição o atributo 'user' que contém o 'id' do usuário provedor do token JWT passado no cabeçalho da requisição
+        req.user = { id:decoded.sub }
+        return next()
+    }catch(err){
+        return res.status(400).json(err)
+    }
+    
 
-    // Adiciona à requisição o atributo 'user' que contém o 'id' do usuário provedor do token JWT passado no cabeçalho da requisição
-    req.user = { id:decoded.sub }
-
-    return next()
 }
 
 module.exports = { UserAuthenticated }
